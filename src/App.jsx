e1c9e1e4fcb9d352fa53e1a8ab8877521a1958bb
Muhammadullo <mnastulloev@gmail.com>
2023-12-26 11:43:14 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,22 @@ import './App.css'
 import 'animate.css';
 
 
+// aos
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 // mui icon
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Button from '@mui/material/Button';
 import MenuIcon from '@mui/icons-material/Menu';
+import { useEffect } from 'react';
 
 function App() {
+
+// aos
+  useEffect(() => {
+    AOS.init();
+  }, [])
 
   return (
     <>
@@ -42,27 +52,53 @@ function App() {
 
 
       {/* section1 */}
-      <section className='section1 w-[100%] h-[80vh] md:flex justify-around items-center'>
+      <section className='section1 w-[100%] h-[80vh] md:flex justify-around items-center '>
 
         <div className='md:flex justify-around items-center'>
-         
+
           {/* left */}
           <div className='mob:flex  flex-col items-center justify-center mob:mb-[20px] md:items-start'>
             <p className='text-[#D77474] font-bold   animate__animated animate__slideInLeft'>Pompeo Pottery</p>
             <div className='flex items-center'>
               <h1 className='md:text-[50px] mob:text-[30px]  font-bold animate__animated animate__slideInLeft'>Unique Porcelain</h1>
-              <img  src='src/assets/&.png'/>
+              <img src='src/assets/&.png' />
             </div>
             <h1 className='md:text-[50px] mob:text-[30px] font-bold  animate__animated animate__rotateInUpRight'>Stone Collection</h1>
             <p className='text-[#6C6C6C] mob:text-[10px] md:text-[18px]'>Unique & modern pottery made by our master in porcelain & stones</p>
-            <Button variant="contained" sx={{height:"50px", background:"#AC1313", marginTop:"20px"}}>SHOP COLLECTION</Button>
+            <Button variant="contained" sx={{ height: "50px", background: "#AC1313", marginTop: "20px" }}>SHOP COLLECTION</Button>
           </div>
 
           {/* right */}
           <div>
-            <img src='src/assets/image (17).png'/>
+            <img src='src/assets/image (17).png' />
           </div>
         </div>
+      </section>
+
+
+
+      {/* section2 */}
+      <section className='mt-[40px]'>
+
+        {/* title */}
+        <div  className='flex flex-col items-center justify-center '>
+          <p className='text-[#D77474] font-bold   animate__animated animate__slideInLeft'>Pompeo Pottery</p>
+          <div className='flex items-center'>
+            <h1 className='md:text-[50px] mob:text-[30px]  font-bold animate__animated animate__slideInLeft'>Porcelain</h1>
+            <img src='src/assets/&.png' />
+            <h1 className='md:text-[50px] mob:text-[30px]  font-bold animate__animated animate__slideInLeft'>Pottery</h1>
+          </div>
+        </div>
+
+        <div data-aos="zoom-in-down" className='flex flex-wrap justify-around '>
+          <img src='src/assets/Frame (2).png' className='m-[10px]'  />
+          <img src='src/assets/Frame (3).png' className='m-[10px]' />
+          <img src='src/assets/Frame (4).png' className='m-[10px]' />
+        </div>
+
+
+
+
       </section>
 
     </>
